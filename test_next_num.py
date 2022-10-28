7a6e8c06c22b0222c699226b70e8a14b55350d17
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-10-28 19:12:47 +0100

    Add test file for next_num

---
+++ test_next_num.py
@@ -0,0 +1,5 @@
+import pytest
+
+# TODO: Set a fixed seed to ensure that random number generation is deterministic.
+# TODO: Use multiple seeds and perform +-2 SD test. Use Monte Carlo method?
+# TODO: Look into using Bayesian stats.
