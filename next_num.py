879c5b6557768938731db7c2b7bb6b2e73299bc2
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-10-29 12:34:47 +0100

    Fix comments

--- next_num.py
+++ next_num.py
@@ -6,13 +6,16 @@ import random
 
 class RandomGen(object):
     def __init__(self, random_nums: list, probabilities: list):
-        # Integer values that may be returned by self.next_num().
-        # Example: [-1, 0, 1, 2, 3]
+        """
+        Args:
+            random_nums: Integer values that may be returned by the random
+                number generator. Example: [-1, 0, 1, 2, 3]
+            probabilities: Probability of each number being returned by the
+                random number generator. Example: [0.01, 0.3, 0.58, 0.1, 0.01]
+        """
+        # TODO: Consider adding a flag to sacrifice floating point precision for speed (using numpy with ints instead of decimal).
         self._random_nums = random_nums
 
-        # Probability of the occurence of each value in _random_nums. Convert
-        # these to decimal floating point arithmetic for precision.
-        # Example: [0.01, 0.3, 0.58, 0.1, 0.01]
         # Convert probabilities into decimal floating point arithmetic for
         # precision to prevent floating point arithmetic errors.
         probabilities = list(str(probability) for probability in probabilities)
@@ -26,7 +29,6 @@ class RandomGen(object):
             cumulative += probability
             self._cum_probabilities.append(cumulative)
 
-        # TODO: Consider adding a flag for these checks.
         if len(self._random_nums) != len(self._probabilities):
             raise ValueError("Length of random_nums and probabilities must be equal.")
 
@@ -36,7 +38,7 @@ class RandomGen(object):
         if self._cum_probabilities[-1] != 1.0:
             raise ValueError("Probabilities must sum to 1.")
 
-    def find_cumulative_probability_index(
+    def find_corresponding_index_of_random_number(
         self, cumulative_probabilities: list, random_roll: float
     ) -> int:
         """
@@ -63,7 +65,7 @@ class RandomGen(object):
         random_roll = random.random()
         # Find the index of the number whose probability range this random roll
         # fits into.
-        number_index = self.find_cumulative_probability_index(
+        number_index = self.find_corresponding_index_of_random_number(
             self._cum_probabilities, random_roll
         )
         return self._random_nums[number_index]
@@ -75,12 +77,12 @@ if __name__ == "__main__":
     random_gen = RandomGen(random_nums, probabilities)
     num_counts = collections.defaultdict(int)
     # Set how many numbers to generate - a larger sample size will generally
-    # converge to the expected probabilities.
+    # converge to the expected probabilities due to the law of large numbers.
     iterations = 10000
     for _ in range(iterations):
         num_counts[random_gen.next_num()] += 1
 
-    # Print the results of the tests.
+    # Print the results of the random number generation.
     print(f"Numbers: {random_nums}")
     print(
         f"Probabilities: {probabilities}\n\nNumbers Generated [Number: Frequency "
